d92abcf076cfd3238ef316928100a3ec2cc0c9d1
Kuro1 <412681778@qq.com>
2020-09-11 16:48:01 +0800

    first commit

---
+++ interruptFrame.lua
@@ -0,0 +1,11 @@
+local interruptFrame = CreateFrame("Frame", nil, UIParent)
+interruptFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+local function interruptEvent(self, event, ...)
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        local timestamp, eventType, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, _, spellID, spellName, _, extraSkillID, extraSkillName = ...
+        if eventType == "SPELL_INTERRUPT" and sourceName == UnitName("player") then
+            SendChatMessage("我tm打断了 >> " .. GetSpellLink(extraSkillID), "SAY") --喊话
+        end
+    end
+end
+interruptFrame:SetScript("onEvent", interruptEvent)
